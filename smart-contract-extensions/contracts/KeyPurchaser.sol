ee4c0367909028c3d1f949b17fc966d0c10ffb50
NickCuso <hardlydiff@gmail.com>
2020-02-16 18:30:56 -0800

    Draft complete, tests TODO

--- smart-contract-extensions/contracts/KeyPurchaser.sol
+++ smart-contract-extensions/contracts/KeyPurchaser.sol
@@ -4,14 +4,23 @@ import 'hardlydifficult-ethereum-contracts/contracts/lifecycle/Stoppable.sol';
 import '@openzeppelin/upgrades/contracts/Initializable.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
 import 'unlock-abi-1-3/IPublicLockV6.sol';
 
-// the user can remove approval to cancel anytime. We could also add a cancel feature but that would be redundant with token allowance.
-  // risk: if the user transfers or cancels themselves, they would naturally expect that also cancels the subscription but it does not.
-  // not sure we have a way to deal with this other than in the front-end, unless we change the lock to support this
+/**
+ * @notice Purchase a key priced in any ERC-20 token - either once or as a regular subscription.
+ * This allows the user to purchase or subscribe to a key with 1 tx (`approve`)
+ * or if the token supports it, with 1 signed message (`permit`).
+ *
+ * The user can remove approval to cancel anytime.
+ *
+ * Risk: if the user transfers or cancels the key, they would naturally expect that also cancels
+ * the subscription but it does not. This should be handled by the frontend.
+ */
 contract KeyPurchaser is Initializable, Stoppable
 {
   using Address for address;
+  using SafeERC20 for IERC20;
 
   // set on initialize and cannot change
   IPublicLock public lock;
@@ -19,6 +28,7 @@ contract KeyPurchaser is Initializable, Stoppable
   uint public maxPurchaseCount;
   uint public renewWindow;
   uint public renewMinFrequency;
+  bool public isSubscription;
 
   // admin can change these anytime
   string public name;
@@ -27,14 +37,17 @@ contract KeyPurchaser is Initializable, Stoppable
   // store minimal history
   mapping(address => uint) timeOfLastPurchase;
 
+  /**
+   * @notice Called once to set terms that cannot change later on.
+   */
   function initialize(
     IPublicLock _lock,
     uint _maxKeyPrice,
     uint _renewWindow,
-    uint _renewMinFrequency
+    uint _renewMinFrequency,
+    bool _isSubscription
   ) public initializer()
   {
-    require(_lock.owner() == msg.sender, 'ONLY_OWNER');
     require(_maxKeyPrice > 0, 'INVALID_MAX_KEYPRICE');
     require(_renewWindow > 0, 'INVALID_RENEW_WINDOW');
     require(_renewMinFrequency > 0, 'INVALID_RENEW_MINFREQUENCY');
@@ -44,8 +57,12 @@ contract KeyPurchaser is Initializable, Stoppable
     maxKeyPrice = _maxKeyPrice;
     renewWindow = _renewWindow;
     renewMinFrequency = _renewMinFrequency;
+    isSubscription = _isSubscription;
   }
 
+  /**
+   * @notice Used by admins to update metadata which may be leveraged by the frontend.
+   */
   function config(
     string memory _name,
     bool _disabled
@@ -56,32 +73,43 @@ contract KeyPurchaser is Initializable, Stoppable
   }
 
   /**
-   * @notice Indicates if this purchaser should be exposed as an option to users.
+   * @notice Indicates if this purchaser should be exposed as an option to users on the frontend.
+   * False does not necessarily mean previous subs will no longer work (see `stopped` for that).
    */
-  function isActive() public returns(bool)
+  function isActive() public view returns(bool)
   {
     return !stopped() && !disabled;
   }
 
-  // anyone can call this
-  // optimistic unlock could monitor this call
+  /**
+   * @notice Called by anyone to purchase or renew a key on behalf of a user.
+   * The user must have ERC-20 spending approved and the purchase must meet the terms
+   * defined during initialization.
+   */
   function purchaseFor(
-    address _user,
+    address payable _user,
     address _referrer,
     bytes memory _data
   ) public
   {
-    uint keyPrice = lock.keyPrice();
-    require(keyPrice <= maxKeyPrice, 'PRICE_TOO_HIGH');
-    uint time = lock.keyExpirationTimestampFor(_user);
-    require(time <= now || time - now <= renewWindow, 'OUTSIDE_RENEW_WINDOW');
-    time = timeOfLastPurchase[_user];
-    require(time < now && now - time >= renewMinFrequency, 'BEFORE_MIN_FREQUENCY');
+    uint temp = lock.keyExpirationTimestampFor(_user);
+    require(temp <= now || temp - now <= renewWindow, 'OUTSIDE_RENEW_WINDOW');
+
+    temp = timeOfLastPurchase[_user];
+    require(isSubscription || temp == 0, 'SINGLE_USE_ONLY');
+    // `now` must be strictly larger than the timestamp of the last block
+    require(now - temp >= renewMinFrequency, 'BEFORE_MIN_FREQUENCY');
+
+    temp = lock.keyPrice();
+    require(temp <= maxKeyPrice, 'PRICE_TOO_HIGH');
 
     IERC20 token = IERC20(lock.tokenAddress());
-    token.transferFrom(_user, address(this), keyPrice);
-    token.approve(address(lock), keyPrice);
-    lock.purchase(keyPrice, _user, _referrer, _data);
+    // We don't need safeTransfer or safeApprove as if these do not work the purchase will fail
+    token.transferFrom(_user, address(this), temp);
+    token.approve(address(lock), temp);
+    lock.purchase(temp, _user, _referrer, _data);
     timeOfLastPurchase[_user] = now;
+
+    // RE events: it's not clear emitting an event adds value over the ones from purchase and the token
   }
 }
--- smart-contract-extensions/contracts/KeyPurchaserFactory.sol
+++ smart-contract-extensions/contracts/KeyPurchaserFactory.sol
@@ -6,35 +6,45 @@ import 'hardlydifficult-ethereum-contracts/contracts/proxies/CloneFactory.sol';
 /**
  * @notice A factory for creating keyPurchasers.
  * This contract acts as a registry to discover purchasers for a lock
- * and by creating each purchaser itself, garantees a consistent implementation.
+ * and by creating each purchaser itself, it's a single tx and this garantees
+ * a consistent implementation and that it was created by the lock owner.
  */
 contract KeyPurchaserFactory
 {
   using CloneFactory for address;
 
-  KeyPurchaser public keyPurchaserTemplate;
+  address public keyPurchaserTemplate;
   mapping(address => address[]) internal lockToKeyPurchasers;
 
   constructor() public
   {
-    keyPurchaserTemplate = new KeyPurchaser();
+    keyPurchaserTemplate = address(new KeyPurchaser());
   }
 
+  /**
+   * @notice Deploys a new KeyPurchaser for a lock and stores the address for reference.
+   */
   function deployKeyPurchaser(
     address _lock,
     uint _maxKeyPrice,
     uint _renewWindow,
-    uint _renewMinFrequency
+    uint _renewMinFrequency,
+    bool _isSubscription
   ) public
   {
-    KeyPurchaser purchaser = KeyPurchaser(keyPurchaserTemplate._createClone());
-    purchaser.initialize(_lock, _maxKeyPrice, _renewWindow, _renewMinFrequency);
-    lockToKeyPurchasers[_lock] = purchaser;
+    require(_lock.owner() == msg.sender, 'ONLY_OWNER');
+    address purchaser = keyPurchaserTemplate._createClone();
+    KeyPurchaser(purchaser).initialize(IPublicLock(_lock), _maxKeyPrice, _renewWindow, _renewMinFrequency, _isSubscription);
+    lockToKeyPurchasers[_lock].push(purchaser);
   }
 
+  /**
+   * @notice Returns all the KeyPurchasers for a given lock.
+   * @dev Some KeyPurchasers in this list may have been disabled or stopped.
+   */
   function getKeyPurchasers(
     address _lock
-  ) public
+  ) public view
     returns (address[] memory)
   {
     return lockToKeyPurchasers[_lock];
